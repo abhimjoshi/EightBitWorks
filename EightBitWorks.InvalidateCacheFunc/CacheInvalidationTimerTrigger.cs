8c423059e1b6d06ddc530a3620f19d40c3c138e2
Abhi Joshi <abhimjoshi@outlook.com>
2023-10-12 21:21:21 +0530

    Made changes in AZ function.

--- EightBitWorks.InvalidateCacheFunc/CacheInvalidationTimerTrigger.cs
+++ EightBitWorks.InvalidateCacheFunc/CacheInvalidationTimerTrigger.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Net.Http.Headers;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
@@ -6,12 +6,54 @@ namespace EightBitWorks.InvalidateCacheFunc;
 
 public static class CacheInvalidationTimerTrigger
 {
+    private const string Url = "https://eightbitworks.com";
+    private const string SubUrl = "/invalidate/cache";
+    
     [Function("CacheInvalidationTimerTrigger")]
-    public static void Run([TimerTrigger("0 */1 * * * *")] MyInfo myTimer, FunctionContext context)
+    public static void Run([TimerTrigger("0 */10 * * * *")] MyInfo myTimer, FunctionContext context)
     {
         var logger = context.GetLogger("CacheInvalidationTimerTrigger");
-        logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-        logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+        var client = new HttpClient();
+
+        try
+        {
+            client.BaseAddress = new Uri(Url);
+
+            // Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // List data response.
+            var response =
+                client.GetAsync(SubUrl)
+                    .Result; // Blocking call! Program will wait here until a response is received or a timeout occurs.
+            if (response.IsSuccessStatusCode)
+            {
+                // Parse the response body.
+                var result =
+                    response.Content.ReadAsStringAsync()
+                        .Result; //Make sure to add a reference to System.Net.Http.Formatting.dll
+
+                logger.LogInformation(result);
+            }
+            else
+            {
+                logger.LogError("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+
+            logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now.ToUniversalTime()}");
+            logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, $"There was an issue while making a REST api call: {ex.Message}");
+        }
+        finally
+        {
+            client.Dispose();
+        }
+        
+        
         
     }
 }
